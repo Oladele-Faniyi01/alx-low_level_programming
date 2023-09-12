b96384ef8163212bfe9bfa85d659df9e38bdd98e
Oladele-Faniyi01 <faniyi.oladele@gmail.com>
2023-09-11 19:19:49 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-
 #include<stdio.h>
 /**
- * main -Entry point
- *
- * Return: Alaways 0 (success)
- */
+* main - Entry point
+*
+* Return: Alaways 0 (success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
-}	
+}
