82cf541d114a226f66c3b356dcde6cae402643cd
Oladele-Faniyi01 <faniyi.oladele@gmail.com>
2023-09-11 19:24:38 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Alaways 0 (success)
+*/
+int main(void)
+{
+puts("\"with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
