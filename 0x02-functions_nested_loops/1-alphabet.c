f241e10358f4da4bb652771581a833874c201601
Oladele-Faniyi01 <faniyi.oladele@gmail.com>
2023-09-12 17:17:13 -0700

    Lowercase alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
+#include "main.h"
 /**
 * main -Entry point
 *
 * Return: Always 0 (success)
 */
-int main(void)
+void print_alphabet(void)
 {
 char alphabet;
 for (alphabet = 'a'; alphabet <= 'z' ; alphabet++)
